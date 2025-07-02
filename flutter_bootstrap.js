0f56a184df34f1432723a5e0f00345210befebd1
MMorales <marlon.morales@kuik.tech>
2025-07-02 19:25:14 +0200

    Disable service worker and manifest link for debugging purposes

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,7 +38,8 @@ _flutter.buildConfig = {"engineRevision":"1425e5e9ec5eeb4f225c401d8db69b860e0fde
 
 
 _flutter.loader.load({
-  serviceWorkerSettings: {
-    serviceWorkerVersion: "3419251907"
-  }
+  // Deshabilitando Service Worker para evitar errores de cache
+  // serviceWorkerSettings: {
+  //   serviceWorkerVersion: "3419251907"
+  // }
 });
